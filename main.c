fc3239ba92224dfc4cfd556425d8c0fe04c63945
nathasha567 <as20240567@sci.sjp.ac.lk>
2025-10-23 17:28:13 +0530

    define global variables

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_CITIES 30
+#define MAX_DELIVERIES 50
 #define FUEL_PRICE 310.0
+
+//global variables
+char cities[MAX_CITIES][50]; //  length of city name is 50 characters
+int distance[MAX_CITIES][MAX_CITIES];
+int cityCount = 0;
+
+char vehicleTypes[3][20] = {"Van", "Truck", "Lorry"};
+int vehicleCapacity[3] = {1000, 5000, 10000};
+int vehicleRate[3] = {30, 40, 80};
+int vehicleSpeed[3] = {60,50, 45};
+int vehicleEfficiency[3] = {12, 6,4 };
 
 int main()
 {
