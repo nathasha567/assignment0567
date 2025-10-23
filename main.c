45e324a5ad5e98895bc8caefc28018d6827084b9
nathasha567 <as20240567@sci.sjp.ac.lk>
2025-10-23 17:37:12 +0530

    add display menu function

--- main.c
+++ main.c
@@ -17,6 +17,20 @@ int vehicleRate[3] = {30, 40, 80};
 int vehicleSpeed[3] = {60,50, 45};
 int vehicleEfficiency[3] = {12, 6,4 };
 
+void displayMenu(){
+    printf("LOGISTIC MANAGEMENT SYSTEM\n\n");
+    printf("1. Add City\n");
+    printf("2. Rename City\n");
+    printf("3. Remove City\n");
+    printf("4. Display Cities\n");
+    printf("5. Input Distance\n");
+    printf("6. Display Distance Table\n");
+    printf("7. Process Delivery Request\n");
+    printf("8. Find Least-Cost Route\n");
+    printf("9. View Reports\n");
+    printf("10. Exit\n");
+}
+
 int main()
 {
 
